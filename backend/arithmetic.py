d51fff538705700236f70e42570304cd4638ce7d
Irakli Gudiashvili <igudiashvili99@gmail.com>
2025-03-14 18:28:40 +0400

    added method for calculating matrix addittion/subtraction

---
+++ backend/arithmetic.py
@@ -0,0 +1,5 @@
+def add_matrices(matrix1, matrix2):
+    return [[matrix1[i][j] + matrix2[i][j] for j in range(len(matrix1[0]))] for i in range(len(matrix1))]
+
+def subtract_matrices(matrix1, matrix2):
+    return [[matrix1[i][j] - matrix2[i][j] for j in range(len(matrix1[0]))] for i in range(len(matrix1))]
