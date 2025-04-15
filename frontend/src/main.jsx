e559d090a41f7be60be47c6c36cdd7d6e5fd00fe
Irakli Gudiashvili <igudiashvili99@gmail.com>
2025-04-15 18:15:02 +0400

    added custom scss

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './custom.scss'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom"
-import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/css/bootstrap.min.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
