4cb08b6bf7cae259931a858548d6574ec4d838e3
Irakli Gudiashvili <igudiashvili99@gmail.com>
2025-04-15 18:17:52 +0400

    added multiplication

--- frontend/src/calculations/Multiplication.jsx
+++ frontend/src/calculations/Multiplication.jsx
@@ -1,7 +1,224 @@
+import {useState, useEffect } from "react"
+
 function Multiplication(){
-    return <>
-        <h1>Multiplication</h1>
-    </>
+    const [matrix1Rows, setMatrix1Rows] = useState(2)
+    const [matrix1Cols, setMatrix1Cols] = useState(2)
+
+    const [matrix2Rows, setMatrix2Rows] = useState(2)
+    const [matrix2Cols, setMatrix2Cols] = useState(2)
+
+    const [matrix1, setMatrix1] = useState(Array(matrix1Rows).fill().map(() => Array(matrix1Cols).fill('')));
+    const [matrix2, setMatrix2] = useState(Array(matrix2Rows).fill().map(() => Array(matrix2Cols).fill('')));
+
+    
+    const [result, setResult] = useState(null)
+
+    const addRowMatrix1 = () => {
+        setMatrix1Rows(prev => prev + 1);
+        setMatrix1(prevMatrix => {
+          const newMatrix = [...prevMatrix];
+          newMatrix.push(Array(matrix1Cols).fill(''));
+          return newMatrix;
+        });
+      };
+    const addColumnMatrix1 = () => setMatrix1Cols((prev) => prev + 1);
+    const removeRowMatrix1 = () => {
+        if(matrix1Rows > 1){
+            setMatrix1Rows((prev) => prev - 1);
+        }
+    }
+    const removeColumnMatrix1 = () => {
+        if(matrix1Cols > 1){
+            setMatrix1Cols((prev) => prev - 1);
+        }
+    };
+
+    const addRowMatrix2 = () => {
+        setMatrix2Rows(prev => prev + 1);
+        setMatrix2(prevMatrix => {
+          const newMatrix = [...prevMatrix];
+          newMatrix.push(Array(matrix2Cols).fill(''));
+          return newMatrix;
+        });
+      };
+    const addColumnMatrix2 = () => setMatrix2Cols((prev) => prev + 1);
+    const removeRowMatrix2 = () => {
+        if(matrix2Rows > 1){
+            setMatrix2Rows((prev) => prev - 1);
+        }
+    };
+    const removeColumnMatrix2 = () => {
+        if(matrix2Cols > 1){
+            setMatrix2Cols((prev) => prev - 1);
+        }
+    };
+
+    useEffect(() => {
+        setMatrix1(prevMatrix => {
+            return Array.from({ length: matrix1Rows }, (_, rowIndex) =>
+                Array.from({ length: matrix1Cols }, (_, colIndex) =>
+                    (prevMatrix[rowIndex]?.[colIndex] !== undefined ? prevMatrix[rowIndex][colIndex] : '')
+                )
+            );
+        });
+    }, [matrix1Rows, matrix1Cols]);
+    
+    useEffect(() => {
+        setMatrix2(prevMatrix => {
+            return Array.from({ length: matrix2Rows }, (_, rowIndex) =>
+                Array.from({ length: matrix2Cols }, (_, colIndex) =>
+                    (prevMatrix[rowIndex]?.[colIndex] !== undefined ? prevMatrix[rowIndex][colIndex] : '')
+                )
+            );
+        });
+    }, [matrix2Rows, matrix2Cols]);
+
+    const handleMatrix1Change = (rowIndex, colIndex, value) => {
+        const newMatrix1 = matrix1.map((row, rIndex) =>
+            rIndex === rowIndex
+                ? row.map((cell, cIndex) => (cIndex === colIndex ? value : cell))
+                : row
+        );
+
+        setMatrix1(newMatrix1);
+    }
+
+    const handleMatrix2Change = (rowIndex, colIndex, value) => {
+        const newMatrix2 = matrix2.map((row, rIndex) =>
+            rIndex === rowIndex
+                ? row.map((cell, cIndex) => (cIndex === colIndex ? value : cell))
+                : row
+        );
+
+        setMatrix2(newMatrix2);
+    }
+
+    const isMatrixFilled = (matrix) => {
+        return matrix.every(row => row.every(cell => cell.trim() !== ''));
+    }
+
+    const generateResult = () => {
+        if(matrix1Cols == matrix2Rows){
+            console.log('Calculating')
+
+            const parseMatrix = (matrix) =>
+                matrix.map(row => row.map(cell => parseFloat(cell)));
+    
+            const requestData = {
+                matrix1: parseMatrix(matrix1),
+                matrix2: parseMatrix(matrix2)
+            };
+
+            console.log("requestData: ", requestData)
+
+            fetch("http://127.0.0.1:8000/multiplication", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(requestData)
+            })
+            .then((response) => response.json())
+            .then((data) => {
+                setResult(data.result)
+            })
+            .catch((error) => {
+                setResult(error)
+            });
+        } else {
+            console.log('Error: Error: Matrix1 columns must match Matrix2 rows')
+            setResult('Error: Error: Matrix1 columns must match Matrix2 rows')
+        }
+    }
+
+    return <div className="container d-flex flex-column justify-content-center align-items-center py-3">
+        {/* Matrix 1 */}
+        <div className="px-5 py-3">
+            {Array.from({ length: matrix1Rows }).map((_, rowIndex) => (
+                <div className="d-flex py-2" key={`row1-${rowIndex}`}>
+                    {Array.from({ length: matrix1Cols }).map((_, colIndex) => (
+                        <input 
+                            key={`matrix1-${rowIndex}-${colIndex}`}
+                            className="form-control form-control-sm mx-3"
+                            value={matrix1[rowIndex][colIndex]}
+                            onChange={(e) =>
+                                handleMatrix1Change(rowIndex, colIndex, e.target.value)
+                            }
+                        />
+                    ))}
+                </div>
+            ))}
+
+            <div className="d-flex justify-content-between py-2 ">
+                <div>
+                    <button onClick={addRowMatrix1} className="btn btn-success btn-sm">Add Row</button>
+                    <button onClick={removeRowMatrix1} className="btn btn-danger btn-sm mx-3">Remove Row</button>
+                </div>
+                <div>
+                    <button onClick={addColumnMatrix1} className="btn btn-success btn-sm mx-3">Add Column</button>
+                    <button onClick={removeColumnMatrix1} className="btn btn-danger btn-sm">Remove Column</button>
+                </div>
+            </div>
+        </div>
+
+        {/* Matrix 2 */}
+        <div className="px-5 py-3">
+            {Array.from({ length: matrix2Rows }).map((_, rowIndex) => (
+                <div className="d-flex py-2" key={`row1-${rowIndex}`}>
+                    {Array.from({ length: matrix2Cols }).map((_, colIndex) => (
+                    <input
+                        key={`matrix2-${rowIndex}-${colIndex}`}
+                        className="form-control form-control-sm mx-3"
+                        value={matrix2[rowIndex][colIndex]}
+                        onChange={(e) =>
+                            handleMatrix2Change(rowIndex, colIndex, e.target.value)
+                        }
+                    />
+                    ))}
+                </div>
+            ))}
+
+            <div className="d-flex justify-content-between py-2 ">
+                <div>
+                    <button onClick={addRowMatrix2} className="btn btn-success btn-sm">Add Row</button>
+                    <button onClick={removeRowMatrix2} className="btn btn-danger btn-sm mx-3">Remove Row</button>
+                </div>
+                <div>
+                    <button onClick={addColumnMatrix2} className="btn btn-success btn-sm mx-3">Add Column</button>
+                    <button onClick={removeColumnMatrix2} className="btn btn-danger btn-sm">Remove Column</button>
+                </div>
+            </div>
+        </div>
+
+        <button className="btn btn-info" onClick={generateResult} disabled={!isMatrixFilled(matrix1) || !isMatrixFilled(matrix2) || matrix1Cols !== matrix2Rows}>
+            Calc()
+        </button>
+
+        <div className="">
+            <h3 className="py-3">Result</h3>
+
+            {/* Result Matrix */}
+            <div className="px-5 py-3">
+                {result && Array.isArray(result) ? (
+                    <table className="table table-bordered text-center">
+                        <tbody>
+                            {result.map((row, rowIndex) => (
+                                <tr key={`result-row-${rowIndex}`}>
+                                    {row.map((cell, colIndex) => (
+                                        <td key={`result-cell-${rowIndex}-${colIndex}`} className="px-3 py-2">
+                                            {cell}
+                                        </td>
+                                    ))}
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                ) : (
+                    <p>{result}</p>
+                )}
+            </div>
+        </div>
+    </div>
 }
 
 export default Multiplication
