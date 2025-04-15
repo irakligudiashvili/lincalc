6312a3cedf298e698a8aeb2bc0e88d7f9e03be26
Irakli Gudiashvili <igudiashvili99@gmail.com>
2025-04-15 18:15:13 +0400

    redesigned sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,10 +1,8 @@
 function Sidebar({setSelectedCalculation}){
-    return <div className="col-3">
-        <ul className="list-group">
-            <li onClick={() => setSelectedCalculation('arithmetic')} className="list-group-item list-group-item-action py-3 pe-5">Arithmetic</li>
-            <li onClick={() => setSelectedCalculation('multiplication')} className="list-group-item list-group-item-action py-3 pe-5">Multiplication</li>
-            <li onClick={() => setSelectedCalculation('inversion')} className="list-group-item list-group-item-action py-3 pe-5">Inversion</li>
-        </ul>
+    return <div className="cursor-pointer list-group list-group-flush bg-primary col-3 rounded-4 h-100">
+        <button onClick={() => setSelectedCalculation('arithmetic')} className="cursor-pointer list-group-item color-light my-2 py-2 fs-5 ps-3 bg-primary text-light text-start">Arithmetic</button>
+        <button onClick={() => setSelectedCalculation('multiplication')} className="cursor-pointer list-group-item color-light my-2 py-2 fs-5 ps-3 bg-primary text-light text-start">Multiplication</button>
+        <button onClick={() => setSelectedCalculation('inversion')} className="cursor-pointer list-group-item color-light my-2 py-2 fs-5 ps-3 bg-primary text-light text-start">Inversion</button>
     </div>
 }
 
