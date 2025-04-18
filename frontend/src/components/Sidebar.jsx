a14dcd4eee16faab6f7ac10f69d6e6b16870131b
Irakli Gudiashvili <igudiashvili99@gmail.com>
2025-04-18 21:13:16 +0400

    refactored sidebar to use custom Dropper component

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,9 +1,16 @@
+import Dropper from "./Dropper";
+
 function Sidebar({setSelectedCalculation}){
-    return <div className="cursor-pointer list-group list-group-flush bg-primary col-3 rounded-4 h-100">
-        <button onClick={() => setSelectedCalculation('arithmetic')} className="cursor-pointer list-group-item color-light my-2 py-2 fs-5 ps-3 bg-primary text-light text-start">Arithmetic</button>
-        <button onClick={() => setSelectedCalculation('multiplication')} className="cursor-pointer list-group-item color-light my-2 py-2 fs-5 ps-3 bg-primary text-light text-start">Multiplication</button>
-        <button onClick={() => setSelectedCalculation('inversion')} className="cursor-pointer list-group-item color-light my-2 py-2 fs-5 ps-3 bg-primary text-light text-start">Inversion</button>
-    </div>
+
+    return (
+        <div className="cursor-pointer list-group list-group-flush bg-primary col-3 rounded-4 py-2 h-100">
+            <button onClick={() => setSelectedCalculation("arithmetic")} className="cursor-pointer list-group-item my-2 py-2 fs-5 ps-3 bg-primary text-light text-start">Arithmetic</button>
+            <button onClick={() => setSelectedCalculation("multiplication")} className="cursor-pointer list-group-item my-2 py-2 fs-5 ps-3 bg-primary text-light text-start">Multiplication</button>
+    
+            <Dropper title="Determinant" optionTitles={["2x2 Matrix", "3x3 Matrix"]} optionValues={["determinant2x2", "determinant3x3"]} onSelect={setSelectedCalculation}/>
+            <Dropper title="Inversion" optionTitles={["2x2 Matrix", "3x3 Matrix"]} optionValues={["inversion2x2", "inversion3x3"]} onSelect={setSelectedCalculation} />
+        </div>
+    );
 }
 
 export default Sidebar
